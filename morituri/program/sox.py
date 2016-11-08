3a6e3266c023a6cd7b2e628162f5738b76416115
Samantha Baldwin <fuhsaz+git@cryptic.li>
2016-11-07 23:54:07 -0500

    add sox.length() and tests, add PeakLevelTestCaseNotExistError

--- morituri/program/sox.py
+++ morituri/program/sox.py
@@ -22,3 +22,24 @@ def peak_level(track_path):
     # relevant captured line looks like:
     # Maximum amplitude: 0.123456
     return float(err.splitlines()[3].split()[2])
+
+def length(track_path):
+    """
+    Accepts a path to a sox-decodable audio file.
+
+    Returns track sample length in sample-pairs derived from sox 'samples read'
+    divided by two (one sample per channel = two simultaneous samples) as an
+    int.
+    Returns None on error.
+    """
+    if not os.path.exists(track_path):
+        logging.warning("SoX peak detection failed: file not found")
+        return None
+    sox = Popen([SOX, track_path, "-n", "stat"], stderr=PIPE)
+    out, err = sox.communicate()
+    if sox.returncode:
+        logging.warning("SoX length calculation failed: " + str(sox.returncode))
+        return None
+    # relevant captured line looks like:
+    # Samples read: 123456
+    return int(err.splitlines()[0].split()[2]) / 2
--- morituri/test/test_program_sox.py
+++ morituri/test/test_program_sox.py
@@ -11,3 +11,24 @@ class PeakLevelTestCase(common.TestCase):
 
     def testParse(self):
         self.assertEquals(0.800018, sox.peak_level(self.path))
+
+class PeakLevelTestCaseNotExistError(common.TestCase):
+    def setUp(self):
+        self.path = os.path.join(os.path.dirname(__file__), 'doesnotexist.flac')
+
+    def testParse(self):
+        self.assertEquals(None, sox.peak_level(self.path))
+
+class LengthTestCase(common.TestCase):
+    def setUp(self):
+        self.path = os.path.join(os.path.dirname(__file__), 'track.flac')
+
+    def testParse(self):
+        self.assertEquals(5880, sox.length(self.path))
+
+class LengthTestCaseNotExistError(common.TestCase):
+    def setUp(self):
+        self.path = os.path.join(os.path.dirname(__file__), 'doesnotexist.flac')
+
+    def testParse(self):
+        self.assertEquals(None, sox.length(self.path))
